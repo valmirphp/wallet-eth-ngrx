90dccc9dbc885b8d624b545aba18cf6f9ee06aea
maiara <maiara.sassi@gmail.com>
2019-02-28 08:59:03 -0300

    resolve conflit

--- src/app/shell/directives/background.directive.ts
+++ src/app/shell/directives/background.directive.ts
@@ -9,9 +9,7 @@ export class BackgroundDirective {
   private index = 1;
 
   constructor(private el: ElementRef) {
-<<<<<<< HEAD
-    this.el.nativeElement.style.transition = 'background-image 1s ease-in-out';
-=======
+
 
     if (window.innerWidth > 400) {
       this.setupBg();
@@ -22,17 +20,15 @@ export class BackgroundDirective {
   private setupBg(): void {
     this.el.nativeElement.style.transition = 'background-image 1s ease-in-out';
     this.el.nativeElement.style.backgroundSize = 'cover';
->>>>>>> 7704cd1a3e47f9a0170657964fa9fa78cc098cfe
+
 
     this.nextBackground();
 
     setInterval(() => {
       this.nextBackground();
-<<<<<<< HEAD
-    }, 3000);
-=======
+
     }, 5000);
->>>>>>> 7704cd1a3e47f9a0170657964fa9fa78cc098cfe
+
   }
 
   private nextBackground(): void {
