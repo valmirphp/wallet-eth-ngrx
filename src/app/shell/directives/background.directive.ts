7704cd1a3e47f9a0170657964fa9fa78cc098cfe
Valmir Barbosa <valmir.php@gmail.com>
2019-02-28 08:50:03 -0300

    Fix BackgroundDirective

--- src/app/shell/directives/background.directive.ts
+++ src/app/shell/directives/background.directive.ts
@@ -9,13 +9,22 @@ export class BackgroundDirective {
   private index = 1;
 
   constructor(private el: ElementRef) {
+
+    if (window.innerWidth > 400) {
+      this.setupBg();
+    }
+
+  }
+
+  private setupBg(): void {
     this.el.nativeElement.style.transition = 'background-image 1s ease-in-out';
+    this.el.nativeElement.style.backgroundSize = 'cover';
 
     this.nextBackground();
 
     setInterval(() => {
       this.nextBackground();
-    }, 3000);
+    }, 5000);
   }
 
   private nextBackground(): void {
