930eeff78f1cb3e38835f92684d9614feac06607
maiara <maiara.sassi@gmail.com>
2019-02-28 08:53:21 -0300

    list account init

---
+++ src/app/shell/directives/background.directive.ts
@@ -0,0 +1,28 @@
+import {Directive, ElementRef} from '@angular/core';
+
+
+@Directive({
+  selector: '[weBackground]'
+})
+export class BackgroundDirective {
+
+  private index = 1;
+
+  constructor(private el: ElementRef) {
+    this.el.nativeElement.style.transition = 'background-image 1s ease-in-out';
+
+    this.nextBackground();
+
+    setInterval(() => {
+      this.nextBackground();
+    }, 3000);
+  }
+
+  private nextBackground(): void {
+    this.el.nativeElement.style.backgroundImage = `url(/assets/images/bgs/ethereum-background-${this.index}.jpg)`;
+    this.index++;
+    this.index = this.index > 4 ? 1 : this.index;
+    console.log('nextBg', this.el.nativeElement.style.backgroundImage);
+  }
+
+}
